02cec69eee51a729f800daba216bcb7bc9282104
achenson <45999022+Achenson@users.noreply.github.com>
2019-11-26 10:00:16 +0100

    Updates

---
+++ precache-manifest.a41fc5f5cc152c7f087f2a3c8217e55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06dff3a676889aaddd489d57a04b756",
+    "url": "/Achenson/random-quote-react/index.html"
+  },
+  {
+    "revision": "5b1a363134e99ec5a054",
+    "url": "/Achenson/random-quote-react/static/css/main.3119815f.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/Achenson/random-quote-react/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "5b1a363134e99ec5a054",
+    "url": "/Achenson/random-quote-react/static/js/main.8113112b.chunk.js"
+  },
+  {
+    "revision": "c664e3d82aa94e1d27de",
+    "url": "/Achenson/random-quote-react/static/js/runtime~main.226a5946.js"
+  }
+]);
